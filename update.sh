556d53b8082efedb5b980ad0b19016abbc22e86a
stock169 <58556976+stock169@users.noreply.github.com>
2024-11-14 19:38:27 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 apt update
 apt-get install dialog apt-utils tzdata
 pip install --upgrade pip
-wget 
+wget https://raw.githubusercontent.com/stock169/AutoUpdateJdCookie/refs/heads/main/requirements.txt
 pip install -r requirements.txt
 playwright install chromium
 playwright install-deps
