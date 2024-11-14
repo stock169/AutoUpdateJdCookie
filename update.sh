9643348acfe351169d3d46f88f203853363e6e49
stock169 <58556976+stock169@users.noreply.github.com>
2024-11-14 19:46:46 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 apt update
-apt-get install dialog apt-utils tzdata
+apt-get install dialog apt-utils
 pip install --upgrade pip
 wget https://raw.githubusercontent.com/stock169/AutoUpdateJdCookie/refs/heads/main/requirements.txt
 pip install -r requirements.txt
