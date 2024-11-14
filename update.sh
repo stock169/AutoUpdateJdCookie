41ac54ae67202b2367f2a6239ced2774b351c7f0
stock169 <58556976+stock169@users.noreply.github.com>
2024-11-14 19:57:54 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+apt update
+apt-get install dialog apt-utils
+pip install --upgrade pip
+wget https://raw.githubusercontent.com/stock169/AutoUpdateJdCookie/refs/heads/main/requirements.txt
+pip install -r requirements.txt
+playwright install chromium
+playwright install-deps
