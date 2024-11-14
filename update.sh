06826860ffbfbc1d7ac791a8b12bfa371cca66ed
stock169 <58556976+stock169@users.noreply.github.com>
2024-11-14 19:28:20 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,7 @@
+apt-get install dialog apt-utils tzdata
+pip install --upgrade pip
+wget https://raw.githubusercontent.com/icepage/AutoUpdateJdCookie/refs/heads/main/requirements.txt
+pip install -r requirements.txt
+playwright install chromium
+playwright install-deps
+
