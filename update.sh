70102e3afcd8399c6b08679e6138d538e727014a
stock169 <58556976+stock169@users.noreply.github.com>
2024-12-01 09:47:38 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 apt update
-apt -y upgrade
 apt-get -y install dialog apt-utils
+apt -y upgrade
 pip install --upgrade pip
 rm requirements.txt
 wget https://raw.githubusercontent.com/stock169/AutoUpdateJdCookie/refs/heads/main/requirements.txt
