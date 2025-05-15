7b4c3a4619354b291b13f10a31eeada655ea1469
stock169 <58556976+stock169@users.noreply.github.com>
2025-05-15 09:50:43 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@ apt update
 apt-get -y install dialog apt-utils iputils-ping net-tools openssh-server nano
 mkdir /run/sshd
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "/usr/sbin/sshd -D" > /usr/sbin/ssh.sh
+chmod -755 /usr/sbin/ssh.sh
 apt -y upgrade
 pip install --upgrade pip
 rm requirements.txt
